7c22a8ffb3b77a0bdb31362521ecbe3baf152ebf
Nikunja Sahoo <nikunja@Nikunjas-MacBook-Pro.local>
2020-10-05 16:50:08 +0530

    Add Java class Application.java

---
+++ src/main/java/learn/tech/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package learn.tech;
+
+/**
+ * @author nikunja
+ *
+ */
+public class Application {
+	
+	Application(){
+		System.out.println("Application java initialized.");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Application class.");
+	}
+
+}
